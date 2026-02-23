d00adcd374e26c0da0c683c176ec0172be63137d
Z <z-jinke@qq.com>
2026-02-24 02:58:08 +0800

    更新

--- Js/Kugou.js
+++ Js/Kugou.js
@@ -1,83 +1,101 @@
 let url = $request.url;
 let body = $response.body;
 
-if (/youth\/v1\/ab\/tmeab\?ab_source=TMEAB0WelcomePage0type/.test(url)) {
+try {
+    let obj = JSON.parse(body);
+    let data = obj?.data || {};
+
     try {
-        let obj = JSON.parse(body);
-        function cleanBasicUI(data) {
-            for (let key in data) {
-                let module = data[key];
-                if (!module || !module.mapParams) continue;
-                let params = module.mapParams;
-                if ([
-                    'TMEAB0WelcomePage0type',
-                    'TMEAB0RECsources0type',
-                    'newsongradio',
-                    'soundeffect',
-                    'importlist',
-                    'TMEAB0OfflineH5'
-                ].includes(key)) {
-                    for (let p of ['show','is_show','sound_effect_show','guideanimation','is_on']) {
-                        if (p in params) params[p] = "0";
-                    }
-                }
-                if (key === 'TMEAB0TaskCenterEnter0type') {
-                    for (let p of ['TFC','style','TaskDone','mypage']) params[p] = "0";
-                }
-                if (key === 'TMEAB0EarningTab0type') {
-                    for (let p of ['Earning_Tab','Animation']) params[p] = "0";
-                }
-                if (key === 'TMEAB0FreemiumModelTouchpoint0type') {
-                    for (let p in params) params[p] = "0";
-                }
-                if (['TMEAB0signin0type','TMEAB0freesignin0type'].includes(key)) {
-                    for (let p in params) params[p] = "0";
-                }
-                if (key === 'defaultpic') {
-                    params.defaultpic = "0";
-                }
+        if (/youth\/v1\/ab\/tmeab\?ab_source=TMEAB0WelcomePage0type/.test(url)) {
+
+            if (data.TMEAB0TaskCenterEnter0type?.mapParams) {
+                Object.keys(data.TMEAB0TaskCenterEnter0type.mapParams)
+                    .forEach(k => data.TMEAB0TaskCenterEnter0type.mapParams[k] = "0");
+            }
+            if (data.TMEAB0EarningTab0type?.mapParams) {
+                data.TMEAB0EarningTab0type.mapParams.Earning_Tab = "0";
+                data.TMEAB0EarningTab0type.mapParams.Animation = "0";
+            }
+            if (data.TMEAB0OfflineH5?.mapParams) {
+                data.TMEAB0OfflineH5.mapParams.is_on = "0";
+            }
+            if (data.TMEAB0signin0type?.mapParams) {
+                Object.keys(data.TMEAB0signin0type.mapParams)
+                    .forEach(k => data.TMEAB0signin0type.mapParams[k] = "0");
+            }
+            if (data.TMEAB0freesignin0type?.mapParams) {
+                Object.keys(data.TMEAB0freesignin0type.mapParams)
+                    .forEach(k => data.TMEAB0freesignin0type.mapParams[k] = "0");
+            }
+            if (data.newsongradio?.mapParams) data.newsongradio.mapParams.is_show = "0";
+            if (data.importlist?.mapParams) data.importlist.mapParams.guideanimation = "0";
+            if (data.aiwritesong?.mapParams) data.aiwritesong.mapParams.aiwritesong = "0";
+
+            if (data.TMEAB0FreemiumModelTouchpoint0type?.mapParams) {
+                Object.keys(data.TMEAB0FreemiumModelTouchpoint0type.mapParams)
+                    .forEach(k => data.TMEAB0FreemiumModelTouchpoint0type.mapParams[k] = "0");
+            }
+            if (data.TMEAB0GNBad0splashexpcontrol?.mapParams) {
+                data.TMEAB0GNBad0splashexpcontrol.mapParams.front_5s = "0";
+            }
+            if (data.TMEAB0fmBidding?.mapParams) {
+                data.TMEAB0fmBidding.mapParams.allowRepeatRequest = "0";
+                data.TMEAB0fmBidding.mapParams.rewardAdLocalCache = "0";
+                data.TMEAB0fmBidding.mapParams.cacheCount = "0";
+                data.TMEAB0fmBidding.mapParams.rewardReqInterval = "999999";
+                data.TMEAB0fmBidding.mapParams.rewardRealTimeReqInterval = "999999";
+            }
+            if (data.emptyqueue?.mapParams) {
+                data.emptyqueue.mapParams.recomend = "0";
             }
         }
-        cleanBasicUI(obj.data);
-        body = JSON.stringify(obj);
     } catch (e) {
-        console.log("处理失败 " + e.message);
+        console.log("error: " + e);
     }
-}
 
-if (/youth\/v1\/ab\/tmeab\?ab_source=ysenter/.test(url)) {
     try {
-        let obj = JSON.parse(body);
-        function cleanAllAds(data) {
-            for (let key in data) {
-                let module = data[key];
-                if (!module || !module.mapParams) continue;
-                let params = module.mapParams;
-                for (let p in params) params[p] = "0";
-                if ([
-                    'SelfpageUI',
-                    'ExplorePageV5',
-                    'MusicHall',
-                    'TMEAB0landscapemode0type'
-                ].includes(key)) {
-                    data[key] = [];
-                }
-                if ([
-                    'cube',
-                    'SearchListV2',
-                    'changtingdaily',
-                    'ringtone',
-                    'collect'
-                ].includes(key)) {
-                    for (let p in params) params[p] = "0";
-                }
+        if (/youth\/v1\/ab\/tmeab\?ab_source=ysenter/.test(url)) {
+
+            if (data["TMEAB0ad0type"]?.mapParams) {
+                Object.keys(data["TMEAB0ad0type"].mapParams)
+                    .forEach(k => data["TMEAB0ad0type"].mapParams[k] = "0");
+            }
+            if (data["TMEAB0FreemiumExposureFix0type"]?.mapParams) {
+                Object.keys(data["TMEAB0FreemiumExposureFix0type"].mapParams)
+                    .forEach(k => data["TMEAB0FreemiumExposureFix0type"].mapParams[k] = "0");
+            }
+            if (data["TMEAB0FreemiumExposureFix0typeV2"]?.mapParams) {
+                Object.keys(data["TMEAB0FreemiumExposureFix0typeV2"].mapParams)
+                    .forEach(k => data["TMEAB0FreemiumExposureFix0typeV2"].mapParams[k] = "0");
+            }
+            if (data["TMEAB0vipplay0type"]?.mapParams) {
+                Object.keys(data["TMEAB0vipplay0type"].mapParams)
+                    .forEach(k => data["TMEAB0vipplay0type"].mapParams[k] = "0");
+            }
+
+            if (data["cube"]?.mapParams) {
+                data["cube"].mapParams.home_mode = "1";
+                data["cube"].mapParams.play_page_mode = "1";
+                data["cube"].mapParams.dig_mode = "1";
+                data["cube"].mapParams.row_num = "3";
+            }
+
+            if (data["TMEAB0audition0type"]?.mapParams) {
+                data["TMEAB0audition0type"].mapParams.secs = "3000";
+            }
+            if (data["TMEAB0vipcenter0type"]?.mapParams) {
+                data["TMEAB0vipcenter0type"].mapParams.vipcenter_exposure = "0";
+            }
+            if (data["viphide"]?.mapParams) {
+                data["viphide"].mapParams.song_mode_unvipsong = "0";
             }
         }
-        cleanAllAds(obj.data);
-        body = JSON.stringify(obj);
     } catch (e) {
-        console.log("处理失败 " + e.message);
+        console.log("error: " + e);
     }
-}
 
-$done({ body });
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    console.log("error: " + e);
+    $done({ body });
+}
