fc5ab2916d6738fec56cd4394731d10cfdeba642
Z <z-jinke@qq.com>
2026-03-01 16:19:22 +0800

    更新

--- Js/Polka.js
+++ Js/Polka.js
@@ -1,22 +1,21 @@
 let url = $request.url;
 let body = $response.body;
-let hasError = false;
+let log = [];
 
 function safeSet(obj, key, value) {
     try {
-        if (obj?.[key] !== undefined) obj[key] = value;
+        obj[key] = value;
     } catch (e) {
-        hasError = true;
-        console.log(`处理失败——>${key}`);
+        log.push(`处理失败>>${key}`);
     }
 }
 
 try {
-    let obj = JSON.parse(body);
-
     if (/api\/(ucenter\/users\/(pub|login)|advert\/free\/config)/.test(url)) {
+        let obj = JSON.parse(body);
         const expireTime = new Date('2099-09-09T09:09:00+08:00').getTime();
-        let p = obj?.data?.payInfo;
+
+        let p = obj.data.payInfo;
         safeSet(p, "redFlower", 999);
         safeSet(p, "isSignedBoolean", true);
         safeSet(p, "isCtVipBoolean", true);
@@ -43,7 +42,7 @@ try {
         safeSet(p, "isSigned", 1);
         safeSet(p, "expireDate", expireTime);
 
-        let u = obj?.data?.userInfo;
+        let u = obj.data.userInfo;
         safeSet(u, "status", 1);
         safeSet(u, "isVip", 1);
         safeSet(u, "vipType", 1);
@@ -51,63 +50,69 @@ try {
         safeSet(u, "payVipType", 1);
         safeSet(u, "authType", 1);
 
-        try { delete obj?.data?.allDayConfig?.fmRewards; } catch (e) { hasError = true; }
-    } 
-    else if (/api\/service\/global\/config\/scene/.test(url)) {
-        safeSet(obj?.data, 'showShopEntry', false);
-        safeSet(obj?.data, 'idolTabShow', false);
-        safeSet(obj?.data, 'playingPageCollectPagList', []);
-        safeSet(obj?.data, 'adsNotFinishVipPop4DayInterval', 9999);
-        safeSet(obj?.data, 'AllDialogIntervals', 9999);
-        safeSet(obj?.data?.warmStartDialog, 'count', 0);
-        safeSet(obj?.data, 'offlineFavTipsGuide', 0);
-        safeSet(obj?.data?.downLoadZoneConfig, 'freeTimeRemindTip', 0);
-        safeSet(obj?.data?.iapNewConfig, 'enable', 0);
-        safeSet(obj?.data?.iapConfig, 'enable', 0);
-        safeSet(obj?.data, 'iosAudioSessionManager_ErrAlert', false);
-    } 
-    else if (/api\/service\/home\/index/.test(url)) {
-        obj.data?.moduleList?.forEach((m, i, arr) => { if ([6, 2, 1, 8, 9].includes(m.type)) arr[i] = null; });
-        if (obj.data?.moduleList) obj.data.moduleList = obj.data.moduleList.filter(Boolean);
-    } 
-    else if (/api\/play\/sound\/effect\/list/.test(url)) {
-        obj.data?.list?.forEach(item => {
-            safeSet(item, 'vipType', 'free');
-            safeSet(item, 'audition', 0);
-        });
-    } 
-    else if (/abtest\/ui\/info/.test(url)) {
-        let map = obj?.data?.mapTestInfo;
+        try { delete obj.data.allDayConfig.fmRewards; } catch(e){ log.push("处理失败>>allDayConfig.fmRewards"); }
 
-        let downloadAd = map?.DownloadAd?.mapParams;
-        let downloadAdios = map?.DownloadAdios?.mapParams;
-        let wzDownloadAd = map?.WZDownloadAd?.mapParams;
-        let downloadZoneOpt = map?.DownloadZoneOptimizationAZ?.mapParams;
-        let adExpires = map?.adExpiresFreemodeShowad?.mapParams;
-        let bdLaunchApp = map?.BDLaunchApp?.mapParams;
-        let insertMod = map?.insert?.mapParams;
-        let commentAD = map?.CommentADPosition?.mapParams;
-        let discoverAD = map?.DiscoverADPosition?.mapParams;
-        let clickView = map?.ClickView?.mapParams;
-        let hongbao = map?.hongbao?.mapParams;
-        let dynamicCoins = map?.Dynamiccoins?.mapParams;
-        let wanliu = map?.Wanliu?.mapParams;
-        let mvDialog = map?.bodianmvdialog?.mapParams;
-        let voiceRemind = map?.FreeModVoiceReminder?.mapParams;
-        let mvTry = map?.MvTryShowAds?.mapParams;
-
-        let modules = [downloadAd, downloadAdios, wzDownloadAd, downloadZoneOpt, adExpires,
-            bdLaunchApp, insertMod, commentAD, discoverAD, clickView, hongbao,
-            dynamicCoins, wanliu, mvDialog, voiceRemind, mvTry];
-
-        modules.forEach(mod => {
-            if (mod) Object.keys(mod).forEach(key => safeSet(mod, key, "0"));
-        });
+        body = JSON.stringify(obj);
     }
+    else if (/api\/service\/global\/config\/scene/.test(url)) {
+        let obj = JSON.parse(body);
+        let d = obj.data;
+        safeSet(d, "showShopEntry", false);
+        safeSet(d, "idolTabShow", false);
+        safeSet(d, "playingPageCollectPagList", []);
+        safeSet(d, "adsNotFinishVipPop4DayInterval", 9999);
+        safeSet(d, "AllDialogIntervals", 9999);
+        safeSet(d.warmStartDialog, "count", 0);
+        safeSet(d, "offlineFavTipsGuide", 0);
+        safeSet(d.downLoadZoneConfig, "freeTimeRemindTip", 0);
+        safeSet(d.iapNewConfig, "enable", 0);
+        safeSet(d.iapConfig, "enable", 0);
+        safeSet(d, "iosAudioSessionManager_ErrAlert", false);
 
-    body = JSON.stringify(obj);
-} catch (e) {
-    hasError = true;
+        body = JSON.stringify(obj);
+    }
+    else if (/api\/service\/home\/index/.test(url)) {
+        let obj = JSON.parse(body);
+        obj.data.moduleList = obj.data.moduleList.filter(item => ![6,2,1,8,9].includes(item.type));
+        body = JSON.stringify(obj);
+    }
+    else if (/api\/play\/sound\/effect\/list/.test(url)) {
+        let obj = JSON.parse(body);
+        let list = obj.data.list;
+        for (let i = 0; i < list.length; i++) {
+            safeSet(list[i], "vipType", 'free');
+            safeSet(list[i], "audition", 0);
+        }
+        body = JSON.stringify(obj);
+    }
+    else if (/abtest\/ui\/info/.test(url)) {
+        let obj = JSON.parse(body);
+        let map = obj.data.mapTestInfo;
+        let modules = [
+            map.DownloadAd.mapParams,
+            map.DownloadAdios.mapParams,
+            map.WZDownloadAd.mapParams,
+            map.DownloadZoneOptimizationAZ.mapParams,
+            map.adExpiresFreemodeShowad.mapParams,
+            map.BDLaunchApp.mapParams,
+            map.insert.mapParams,
+            map.CommentADPosition.mapParams,
+            map.DiscoverADPosition.mapParams,
+            map.ClickView.mapParams,
+            map.hongbao.mapParams,
+            map.Dynamiccoins.mapParams,
+            map.Wanliu.mapParams,
+            map.bodianmvdialog.mapParams,
+            map.FreeModVoiceReminder.mapParams,
+            map.MvTryShowAds.mapParams
+        ];
+        for (let mod of modules) {
+            for (let key in mod) safeSet(mod, key, "0");
+        }
+        body = JSON.stringify(obj);
+    }
+} catch(e) {
+
 }
 
-$done({ body });
+$done({ body, log });
