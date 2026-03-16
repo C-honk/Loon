45db5bd1cb2447c761a0886db30782ff0c9b6af9
Z <z-jinke@qq.com>
2026-03-16 18:33:50 +0800

    更新

--- Js/iPInfo.js
+++ Js/iPInfo.js
@@ -1,43 +1,6 @@
-const scriptName = "节点信息查询";
-const countryMap = {
-    "HK": "香港",
-    "TW": "台湾",
-    "KR": "韩国",
-    "JP": "日本",
-    "DE": "德国",
-    "FR": "法国",
-    "GB": "英国",
-    "US": "美国",
-    "SG": "新加坡",
-    "AU": "澳大利亚",
-    "CA": "加拿大",
-    "RU": "俄罗斯",
-    "IN": "印度",
-    "IT": "意大利",
-    "ES": "西班牙",
-    "BR": "巴西",
-    "NL": "荷兰",
-    "CH": "瑞士",
-    "SE": "瑞典",
-    "NO": "挪威",
-    "DK": "丹麦",
-    "FI": "芬兰",
-    "PL": "波兰",
-    "UA": "乌克兰",
-    "MX": "墨西哥",
-    "AE": "阿联酋",
-    "SA": "沙特阿拉伯",
-    "TR": "土耳其",
-    "AR": "阿根廷",
-    "ZA": "南非",
-    "NZ": "新西兰",
-    "MY": "马来西亚",
-    "TH": "泰国",
-    "PH": "菲律宾",
-    "VN": "越南",
-    "ID": "印度尼西亚"
-};
+// 2026-3-16
 
+const scriptName = "节点信息查询";
 (async () => {
     const inputParams = $environment.params;
     const nodeName = inputParams.node;
@@ -50,7 +13,7 @@ const countryMap = {
     try {
         const landingInfo = await new Promise((resolve, reject) => {
             const timer = setTimeout(() => reject(new Error("请求超时")), 5000);
-            $httpClient.get({ url: "http://ipinfo.io/json", node: nodeName }, (err, resp, body) => {
+            $httpClient.get({ url: "http://ipwho.is/?lang=zh-CN", node: nodeName }, (err, resp, body) => {
                 clearTimeout(timer);
                 if (err) return reject(new Error("请求失败：" + err.message));
                 try { resolve(JSON.parse(body)); } 
@@ -58,25 +21,21 @@ const countryMap = {
             });
         });
 
-        if (landingInfo?.ip) {
-            let countryName = "";
-            if (landingInfo.country && countryMap[landingInfo.country]) {
-                countryName = countryMap[landingInfo.country];
-            } else {
-                countryName = landingInfo.country || landingInfo.region || "";
-            }
+        if (landingInfo?.success && landingInfo?.ip) {
 
-            landingHtml = 
+            landingHtml =
                 `IP：${landingInfo.ip}<br>` +
-                `所在地：${countryName}<br>` +
-                `${landingInfo.org ? `运营商：${landingInfo.org.replace(/^AS\d+\s*/, "")}<br>` : ""}`;
+                `所在地：${landingInfo.country || ""}<br>` +
+                `${landingInfo.connection?.isp ? `运营商：${landingInfo.connection.isp}<br>` : ""}`;
         }
+
     } catch (err) {
         errorLogs.push(`落地：${err.message}`);
     }
 
     try {
         let entryIp = nodeAddress;
+
         if (!/^\d{1,3}(\.\d{1,3}){3}$/.test(nodeAddress)) {
             const res = await new Promise((resolve, reject) => {
                 const timer = setTimeout(() => reject(new Error("DNS解析超时")), 5000);
@@ -87,6 +46,7 @@ const countryMap = {
                     catch { resolve([]); }
                 });
             });
+
             if (res?.length > 0) entryIp = res[0];
             else errorLogs.push("无法解析IP");
         }
@@ -103,6 +63,7 @@ const countryMap = {
 
         if (entryInfo?.data) {
             const decoded = {};
+
             for (let key in entryInfo.data) {
                 if (typeof entryInfo.data[key] === "string") {
                     try { decoded[key] = decodeURIComponent(escape(entryInfo.data[key])); } 
@@ -112,20 +73,21 @@ const countryMap = {
                 }
             }
 
-            entryHtml = 
+            entryHtml =
                 `IP：${entryIp}<br>` +
                 `所在地：${decoded.city || decoded.province || ""}<br>` +
                 `运营商：${decoded.isp || decoded.operator || ""}<br>`;
         }
+
     } catch (err) {
         errorLogs.push(`入口：${err.message}`);
     }
 
     const html = `
-    <p style="text-align:center; font-family:-apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI'; font-size:16px; line-height:1.1;">
+    <p style="text-align:center; font-family:-apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI'; font-size:16px; line-height:1.4;">
         <br>
         ${entryHtml ? `<span style="color:#FF993A;">入口位置</span><br>${entryHtml}<br>` : ""}
-        ${landingHtml ? `<span style="color:#6792FF;">落地位置</span><br>${landingHtml}<br>` : ""}
+        ${landingHtml ? `<span style="color:#578BFF;">落地位置</span><br>${landingHtml}<br>` : ""}
         选中 ➞ ${nodeName}<br>
         ${errorLogs.length ? `<br><span style="color:red;">${errorLogs.join("<br>")}</span>` : ""}
     </p>`;
